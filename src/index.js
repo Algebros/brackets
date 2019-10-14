c5518de1c613c410dfe4dc455e9ac4b1cc49c4ea
Sofroniy <Sofroniy.m@gmail.com>
2019-10-14 16:52:58 +0300

    feat: completed task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let obj = {};
+  bracketsConfig.forEach(element => {
+    obj[element[0]] = element[1];
+  });
+  
+  let arr = str.split("");
+  let stack = [];
+  for (let i = 0; i < arr.length; i++) {
+    let lastBracket = stack[stack.length-1];
+    if(obj[lastBracket] == arr[i]) {
+      stack.pop();
+    } else {
+      stack.push(arr[i]);
+    }
+  }
+  if(stack.length == 0) return true;
+  return false;
 }
